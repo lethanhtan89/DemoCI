64900f6c5b6ff8f47e1d083584e079fda1141e70
Tan.Le <tan.le.t@galaxytechnology.vn>
2025-10-24 15:36:08 +0700

    - update toát

--- sdk/src/main/java/com/tan/sdk/DemoSDK.kt
+++ sdk/src/main/java/com/tan/sdk/DemoSDK.kt
@@ -5,14 +5,14 @@ import android.widget.Toast
 
 object DemoSDK {
     fun showToast1(context: Context) {
-        Toast.makeText(context, "showToast1", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "showToast111", Toast.LENGTH_SHORT).show()
     }
 
     fun showToast2(context: Context) {
-        Toast.makeText(context, "showToast2", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "showToast222", Toast.LENGTH_SHORT).show()
     }
 
     fun showToast3(context: Context) {
-        Toast.makeText(context, "showToast3", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "showToast333", Toast.LENGTH_SHORT).show()
     }
 }
